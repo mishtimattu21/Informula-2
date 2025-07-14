5d9c1fbcd784c8c8b9b65820ce2625a63122917e
mishmattu <mattumishti21@gmail.com>
2025-07-14 20:37:42 +0530

    contact

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -5,19 +5,18 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import StepsSection from '../components/StepsSection';
 import FeaturesSection from '../components/FeaturesSection';
-import ContactSection from '../components/ContactSection';
 import Footer from '../components/Footer';
 
 const Index = () => {
   return (
     <ThemeProvider>
-      <div className="min-h-screen">
+      <div className="min-h-screen bg-gradient-to-br from-background via-emerald-50/30 to-teal-50/40 dark:from-background dark:via-emerald-950/20 dark:to-teal-950/30">
         <Navigation />
         <HeroSection />
         <AboutSection />
         <StepsSection />
         <FeaturesSection />
-        <ContactSection />
+        {/* <ContactSection /> */}
         <Footer />
       </div>
     </ThemeProvider>
